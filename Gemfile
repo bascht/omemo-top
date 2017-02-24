9561cb1b24f564b8442433f0de093d465dac82b8
Sebastian Schulze <github.com@bascht.com>
2017-02-24 20:28:31 +0000

    First "build it on Travis" iteration.

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ versions = JSON.parse(open('https://pages.github.com/versions.json').read)
 gem 'jekyll', '~> 3.3'
 gem 'github-pages', versions['github-pages'], group: :jekyll_plugin
 gem 'jekyll-theme-cayman'
+gem 'nokogiri'
---
+++ test.rb
@@ -0,0 +1,21 @@
+require "minitest/autorun"
+require "fileutils"
+require "nokogiri"
+
+describe "omemo.top" do
+  before do
+    FileUtils.rm_rf "./_site/"
+    system "jekyll build"
+  end
+
+  describe "After running Jekyll Build" do
+    it "must have an index.html file" do
+      assert_equal true, File.exists?("./_site/index.html")
+    end
+
+    it "should have at least one Client in it" do
+      h = Nokogiri::HTML.parse(open("_site/index.html"))
+      assert_equal h.title, "Are we OMEMO yet?"
+    end
+  end
+end
